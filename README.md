8d640d9b03360ba7777898cae0b47d98b977db93
juanra <jrgavilanes@gmail.com>
2021-09-11 19:32:45 +0200

    first commit

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 ```
 # uvicorn main:app --reload
 ```
+
+## Deploy en Heroku
+
+https://towardsdatascience.com/how-to-deploy-your-fastapi-app-on-heroku-for-free-8d4271a4ab9
+
+```
+heroku login
+heroku create nombre-app
+heroku apps
+git remote -v
+heroku git:remote -a nombre-app
+
+git commit -m "bla bla"
+git push heroku master
+heroku open
+heroku logs
+heroku info
+
+```
+
+https://janrax-translate.herokuapp.com/languages_list
+
+https://janrax-translate.herokuapp.com/translate/
+
+https://janrax-translate.herokuapp.com/docs
--- traductor.py
+++ traductor.py
@@ -1,20 +1,23 @@
 from googletrans import Translator
+
 translator = Translator()
 
-#print(translator.translate('veritas lux mea', src='la', dest='es').text)
 
-def traducir(texto_a_traducir:str, lenguaje_origen:str = "es", lenguaje_destino:str = "en") -> str:
+# print(translator.translate('veritas lux mea', src='la', dest='es').text)
+
+def traducir(texto_a_traducir: str, lenguaje_origen: str = "es", lenguaje_destino: str = "en") -> str:
     """ Traducir un texto dado
-        Ej: traducir("hola me llamo Juan", lenguaje_origen="es", lenguaje_destino="en")
-        :rtype: object
 
+        Ej: traducir("hola me llamo Juan", lenguaje_origen="es", lenguaje_destino="en")
     """
     return translator.translate(texto_a_traducir, src=lenguaje_origen, dest=lenguaje_destino).text
+
 
 def main():
     print(traducir("Hola mi nombre es Juanra"))
     print(traducir("Hola mi nombre es Juanra", lenguaje_destino="it"))
     print(traducir("bla blalalaldkdjflñdafadfaj", lenguaje_destino="it"))
+
 
 if __name__ == '__main__':
     main()
