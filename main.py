4890b82367a1348af84b8e1d20406854144f568f
juanra <jrgavilanes@gmail.com>
2021-09-13 18:35:47 +0200

    comento lineas CORS

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ import traductor
 
 app = FastAPI(title="Api traductor", description="Api traducir utilizando api de Google translator", version="0.2")
 
-origins = [
-    "*",
-    # "http://localhost",
-    # "http://localhost:8080",
-    # "http://127.0.0.1:5500"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = [
+#     "*",
+#     # "http://localhost",
+#     # "http://localhost:8080",
+#     # "http://127.0.0.1:5500"
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.on_event('startup')
